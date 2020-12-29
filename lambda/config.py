07c25abcc6f0cbc2cfc8c6d7fce63cbb151d22c6
1337-server <62730074+1337-server@users.noreply.github.com>
2020-12-29 02:11:08 +0000

    Update config.py

--- lambda/config.py
+++ lambda/config.py
@@ -6,17 +6,16 @@ client_id = "your-api-key-goes-here"
 # Do you want notified of actions
 notify = True
 
-# This is the title for you notifications
-# You can customise them as you need/want
-# {0} is the type of media that has been added
-notify_title = "A new {0} has been added"
+# {0} is the type of media that has been added/removed
+# {1} is the action that has been performed added/removed
+notify_title = "A {0} has been {1}"
 
-# This is the body for you notifications
-# You can customise them as you need/want
-# {0} is the name of the show/movie being added/removed
+# {0} is the name of the show/movie
 # {1} is the type (movie or show)
 # {2} is the list it was added too
-notify_body = "{0} has been added to {2} list"
+# {3} is the action that has been performed added/removed
+notify_body = "{0} has been {3} to {2} list"
+
 
 # Discord notify
 DISCORD_WEBHOOK_ID = ""
